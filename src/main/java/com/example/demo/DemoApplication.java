e02d9e50e0b73177fe1bd28e2bd6463a0194c1ad
조성현 <agllwy116@naver.com>
2022-11-17 12:14:59 +0900

    디펜던시 라이브러리 추가 및 롬북 테스트 클래스 생성

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 어노테이션 : 이것이 있는 클래스가 Base Package
 public class DemoApplication {
 
     public static void main(String[] args) {
